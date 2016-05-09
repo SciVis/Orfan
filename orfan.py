1b1cb520c85d2adece1df35c4fa5047d1cd4dd41
Peter Steneteg <peter@steneteg.se>
2016-05-09 13:28:20 +0200

    Main, added destination switch

--- orfan.py
+++ orfan.py
@@ -9,13 +9,16 @@ if __name__ == '__main__':
         description="Orfan manager",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('-p', '--path', type=str, nargs=1, action="store", dest="path",
+    parser.add_argument('-p', '--path', type=str, required=True, action="store", dest="path",
                         help='Path to data storage')
+
+    parser.add_argument('-d', '--dest', type=str, required=True, action="store", dest="dest",
+                       help='Destination of dataset gallery')
 
     args = parser.parse_args()
     meta = orfan.scraper.scrape(args.path)
     html = orfan.generator.generate(meta)
 
-    with open('/Users/alex/Desktop/test.html', 'w') as f:
+    with open(args.dest, 'w') as f:
         f.write(str(html))
     print(html)
